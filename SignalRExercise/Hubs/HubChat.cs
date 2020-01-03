be03b6b480c69c47af69ae607783931e97cc1f8c
bin zhang <531047332@qq.com>
2020-01-03 10:16:45 +0800

    add singalR project

---
+++ SignalRExercise/Hubs/HubChat.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRExercise.Hubs
+{
+    public class HubChat: Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToCaller(string message)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
